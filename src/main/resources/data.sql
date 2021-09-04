da07bc52e9a8f2edefc9fcd3d51a81772de30927
hsju0202 <hsju0202@gmail.com>
2021-09-04 10:14:37 +0900

    FEAT: data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+--user, room 필요
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (1, 1, "review1", "2021-10-01", 1);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (1, 1, "review1", "2021-10-02", 2);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (1, 1, "review1", "2021-10-03", 3);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (1, 1, "review1", "2021-10-04", 4);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (1, 1, "review1", "2021-10-05", 5);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (2, 2, "review1", "2021-10-06", 1);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (2, 2, "review1", "2021-10-07", 2);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (2, 2, "review1", "2021-10-08", 3);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (2, 2, "review1", "2021-10-09", 4);
+insert into reviews(user_id, room_id, review_content, created_date, review_score) values (2, 2, "review1", "2021-10-10", 5);
